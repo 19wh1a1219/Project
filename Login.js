03a24110fd963d3b632b935e0a0695074c3e90ed
19wh1a1219 <69143670+19wh1a1219@users.noreply.github.com>
2022-03-15 20:24:10 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import axios from 'axios';
-//import { useHistory } from 'react-router-dom';
+import './Dashboard';
+import { useNavigate } from 'react-router-dom';
 import Home from './Home';
 
 export const Login = () => {
+
+  const navigate = useNavigate();
 
   const initialValue = {email:'', password:''}
   const [state, setState] = React.useState(initialValue);
@@ -14,6 +17,10 @@ export const Login = () => {
     const { name, value } = event.target;
     setState({ ...state, [name]: value });
   }
+
+  // const NavigatePage = (event) =>{
+  //   navigate('/');
+  // }
 
   const GetDetails = (event) =>{
     // let history = useHistory();
@@ -39,13 +46,14 @@ export const Login = () => {
       else
       {
         alert("Welcome!!");
-        // history.push('/Home')
+        navigate('/Dashboard', { replace: true });
       }
     })
   }
 
   const handleSubmit = (event) =>{
     //alert("Success!!");
+    //NavigatePage();
     GetDetails();
     event.preventDefault();
   }
@@ -74,3 +82,137 @@ export const Login = () => {
     </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React,{Component} from "react";
+// import axios from "axios";
+// import './Login.css';
+
+
+// //import Home from "./Home";
+// //import Dashboard from "./Dashboard";
+// //import { BrowserRouter as Router } from 'react-router-dom';
+
+// // import IconButton from "@material-ui/core/IconButton";
+// // import InputLabel from "@material-ui/core/InputLabel";
+// // import Visibility from "@material-ui/icons/Visibility";
+// // import InputAdornment from "@material-ui/core/InputAdornment";
+// // import VisibilityOff from "@material-ui/icons/VisibilityOff";
+// // import Input from "@material-ui/core/Input";
+
+// class Login extends Component{
+    
+//     state = {
+//         email: '',
+//         password: '',
+//         showPassword: false,
+//         record: [],
+//         fireErrors: '',
+//     }
+
+//     changeHandler = (event) =>{
+//         this.setState({[event.target.name]: event.target.value})
+//     }
+
+
+//       getDetails = (event) =>{
+//         axios.get('http://localhost:3000/fetch').then((result) => {
+//             this.state.records = result.data;
+//           var flag = 0;
+//         for(var i=0; i<this.state.records.length; i++)
+//         {
+//             if((this.state.records[i].email === this.state.email && this.state.records[i].password == this.state.password))
+//             {
+//               flag = 1;
+//               break;
+//             }
+//         }
+//         console.log(flag);
+//         if(flag == 0)
+//         {
+//         alert("Enter valid login details");
+//         return;
+//         }
+//         else
+//         {
+//         alert("Welcome!!");
+//         }
+//         })
+//       }
+
+//       handleSubmit = (event) =>{
+//         //return this.getDetails();
+//         return this.getDetails();
+        
+//         event.preventDefault()
+//     }
+
+//     // handleClickShowPassword = () => {
+//     //     this.state.showPassword =  !this.state.showPassword;
+//     //   };
+      
+//     //   handleMouseDownPassword = (event) => {
+//     //     event.preventDefault();
+//     //   };
+    
+    
+//     render() {
+//         //const { navigation } = this.props;
+
+//         //let history = useHistory();
+
+//         return(
+//             <>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <input type = "text" 
+//                         className="regField"
+//                         placeholder="Email"
+//                         onChange={this.changeHandler}
+//                         name="email"/><br/><br/>
+
+//                     <input type = "password" 
+//                         className="regField"
+//                         placeholder="Password"
+//                         onChange={this.changeHandler}
+//                         name="password"/>
+
+//                     {/* <Input className="regField"
+//                         name="password"
+//                         type={this.state.showPassword ? "text" : "password"}
+//                         onChange={this.changeHandler}
+//                         value={this.state.password}
+//                         endAdornment={
+//                         <InputAdornment position="end">
+//                             <IconButton
+//                             onClick={this.handleClickShowPassword}
+//                             onMouseDown={this.handleMouseDownPassword}
+//                             >
+//                             {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+//                             </IconButton>
+//                         </InputAdornment>
+//                         }
+//                     /><br/> */}
+
+//                     <input type="submit" className="submitBtn" value="ENTER"/><br/><br/><br/>
+
+//                 </form>
+//             </>
+//         );
+//     }
+// }
+// export default Login;
